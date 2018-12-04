c4cc1ffbcf6158f9442f492f66b49693753862a9
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-04 07:10:07 +0100

    Imports firebase dependencies

--- Animalis/src/app/app.module.ts
+++ Animalis/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire';
+import { AngularFireDatabaseModule } from "angularfire";
+import { AngularFireAuthModule } from "angularfire";
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
