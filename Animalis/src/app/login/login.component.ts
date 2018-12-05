e1f14bd01c1d03e5c98fb4ab9c4cc2722a727875
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-05 21:12:03 +0100

    Implemented login with Google method

--- Animalis/src/app/login/login.component.ts
+++ Animalis/src/app/login/login.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as firebase from  'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) { 
+    
   }
 
   login() {
-    
+    this.afAuth.auth.signInWithRedirect(
+      new firebase.auth.GoogleAuthProvider());
   }
 
 }
