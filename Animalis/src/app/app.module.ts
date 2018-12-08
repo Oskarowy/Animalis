bb10127335c67414be57d2f30e7dafae00c2b384
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-08 20:16:14 +0100

    Created user service with update user method

--- Animalis/src/app/app.module.ts
+++ Animalis/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { AuthService } from './auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -57,7 +58,8 @@ import { AuthGuard } from './auth-guard.service';
   ],
   providers: [
     AuthService,
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
