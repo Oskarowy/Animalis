f9b76d7e2169e16e6916f32b972e018d7e41f54c
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-09 00:25:05 +0100

    Displaying all products from database - basic implementation done

--- Animalis/src/app/product.service.ts
+++ Animalis/src/app/product.service.ts
@@ -1,14 +1,26 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+observableProducts$: Observable<any>;
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) {
+    this.observableProducts$ = this.db.list('/products').snapshotChanges();
+   }
 
   create(product) {
     return this.db.list('/products').push(product);
+  }
+
+  getAll() {
+    return this.observableProducts$.pipe(
+      map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    }));
   }
 }
