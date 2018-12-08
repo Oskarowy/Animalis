3424fe4b7f9535263f0d214ec51ff4f32759f9ff
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-08 23:19:15 +0100

    Populated categories list in new product form

--- Animalis/src/app/category.service.ts
+++ Animalis/src/app/category.service.ts
@@ -1,14 +1,23 @@
+import { map } from 'rxjs/operators';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+observableCategories$: Observable<any>;
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) { 
+    this.observableCategories$ = this.db.list('/categories',
+    ref => ref.orderByChild('name')).snapshotChanges();
+  }
 
   getCategories() {
-    return this.db.list('/categories');
+    return this.observableCategories$.pipe(
+      map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    }));
   }
 }
