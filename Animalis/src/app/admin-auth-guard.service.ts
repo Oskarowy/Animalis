692a50e2b5a6704eebb6a8a2eb8199563275f756
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-08 21:47:21 +0100

    Hide admin-only links in application's navbar

--- Animalis/src/app/admin-auth-guard.service.ts
+++ Animalis/src/app/admin-auth-guard.service.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { CanActivate } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { map, switchMap } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,8 +14,7 @@ export class AdminAuthGuard implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean> {
-    return this.auth.user$
-      .pipe(switchMap(user => this.userService.get(user.uid)),
-      map((appUser: AppUser) => appUser.isAdmin))
+    return this.auth.appUser$.pipe(
+      map((appUser: AppUser) => appUser.isAdmin));
   }
 }
--- Animalis/src/app/auth.service.ts
+++ Animalis/src/app/auth.service.ts
@@ -1,8 +1,11 @@
+import { UserService } from './user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { AppUser } from './models/app-user';
+import { switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,8 @@ export class AuthService {
   
   constructor(
     private afAuth: AngularFireAuth,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private userService: UserService) {
     this.user$ = afAuth.authState;
   }
 
@@ -26,5 +30,16 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .pipe(switchMap(user => {
+        if(user) {
+          return this.userService.get(user.uid);
+        } else {
+          return of(null);
+        }
+      }));
   }
 }
