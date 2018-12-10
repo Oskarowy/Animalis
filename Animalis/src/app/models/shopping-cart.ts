518d01fc74450ca4d0f2f5deafbc208c0c487ff9
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 18:23:18 +0100

    Display total price as a summary for shopping cart page

--- Animalis/src/app/models/shopping-cart.ts
+++ Animalis/src/app/models/shopping-cart.ts
@@ -1,14 +1,32 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class ShoppingCart {
+    items: ShoppingCartItem[] = [];
 
-    constructor(public key: string, public items: ShoppingCartItem[]) {  
+    constructor(
+        public productId: string, 
+        public itemsMap: { [key: string]: ShoppingCartItem }) {  
+        
+            this.itemsMap = itemsMap || {}; 
+
+            for (let prodId in itemsMap) {
+            let item = itemsMap[prodId];
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        }
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+           sum += this.items[productId].totalPrice;
+
+        return sum;
     }
 
     get totalItemsCount() {
         let count = 0;
-        for (let productId in this.items)
-          count += this.items[productId].quantity;
+        for (let productId in this.itemsMap)
+          count += this.itemsMap[productId].quantity;
           
         return count;
     }
