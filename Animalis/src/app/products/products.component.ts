6090ab155d3c03dabf0ba9069955013aad7df66d
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 13:11:06 +0100

    Filtering products by clicking on category name

--- Animalis/src/app/products/products.component.ts
+++ Animalis/src/app/products/products.component.ts
@@ -1,6 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
+import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[];
   categories$;
+  category: string;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) {
+
+    productService
+      .getAll()
+      .pipe(switchMap(products => {
+        this.products = products;
+        return route.queryParamMap;
+      }))
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+
     this.categories$ = categoryService.getAll();
   }
 
