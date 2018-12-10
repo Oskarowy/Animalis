327feaca6ed59fa4d8d9c624e56e488bd726b6be
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 16:16:18 +0100

    Adding products to the cart method

--- Animalis/src/app/shopping-cart.service.ts
+++ Animalis/src/app/shopping-cart.service.ts
@@ -1,5 +1,7 @@
+import { Product } from './models/product';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,14 +20,21 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
-    if(!cartId) {
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    }
-    
-      return this.getCart(cartId);
+    if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let item = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
+    item.snapshotChanges().pipe(take(1)).subscribe((i: any) => {
+      if (i.payload.val()) item.update({ quantity: i.payload.val().quantity + 1});
+      else item.set({ product: product, quantity: 1 });
+    });
   }
 }
