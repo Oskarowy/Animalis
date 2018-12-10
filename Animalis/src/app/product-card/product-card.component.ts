167360629224bd0afe6592e0dc1704622fa48ead
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 16:42:31 +0100

    Displaying quanitity of products below product card - basic implementation

--- Animalis/src/app/product-card/product-card.component.ts
+++ Animalis/src/app/product-card/product-card.component.ts
@@ -10,6 +10,7 @@ import { Product } from '../models/product';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart;
 
   constructor(private cartService: ShoppingCartService) { }
 
@@ -17,5 +18,11 @@ export class ProductCardComponent {
     this.cartService.addToCart(product);
   }
 
+  getQuantity() {
+    if (!this.shoppingCart) return 0;
+    
+    let item = this.shoppingCart.items[this.product.key];
+    return item ? item.quantity : 0;
+  }
 
 }
--- Animalis/src/app/products/products.component.ts
+++ Animalis/src/app/products/products.component.ts
@@ -1,22 +1,27 @@
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../models/product';
 import { switchMap } from 'rxjs/operators';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private shoppingCartService: ShoppingCartService) {
 
     productService
       .getAll()
@@ -33,6 +38,12 @@ export class ProductsComponent {
       });
   }
 
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+      .valueChanges().subscribe(cart => this.cart = cart);
+  }
 
-
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
