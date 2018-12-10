99c82653999086b8a41ecf8153b079b2c5309bfd
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 21:28:07 +0100

    Redirecting user after placing an order to order-success component

--- Animalis/src/app/check-out/check-out.component.ts
+++ Animalis/src/app/check-out/check-out.component.ts
@@ -5,6 +5,7 @@ import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrderService } from '../order.service';
 import { Order } from '../models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -19,15 +20,17 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   userSubscription: Subscription;
 
   constructor (
+    private router: Router,
     private authService: AuthService,
     private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) {
 
   }
   
-  placeOrder() {
+  async placeOrder() {
     let order = new Order(this.userId, this.shipping, this.cart);
-    this.orderService.storeOrder(order);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key])
   } 
   
   async ngOnInit () {
