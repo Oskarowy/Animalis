99030c44ca9f1bb30a16e1bac91b3d627f946b9a
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-10 16:55:30 +0100

    Implemented logic of adding and removing products by buttons below the product card

--- Animalis/src/app/shopping-cart.service.ts
+++ Animalis/src/app/shopping-cart.service.ts
@@ -35,10 +35,18 @@ export class ShoppingCartService {
   }
 
   async addToCart(product: Product) {
+    this.updateItemQuantity(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItemQuantity(product, -1);
+  }
+
+  private async updateItemQuantity(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item = this.getItem(cartId, product.key);
     item.snapshotChanges().pipe(take(1)).subscribe((i: any) => {
-      item.update({ product: product, quantity: (i.payload.exists() ? i.payload.val().quantity : 0) + 1 });
+      item.update({ product: product, quantity: (i.payload.exists() ? i.payload.val().quantity : 0) + change });
     });
   }
 }
