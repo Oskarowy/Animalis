f8c44f911ca4980cfd67d43f11737a5a0f0b6114
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-11 21:08:09 +0100

    Moved all of shopping-connected components into app/shopping/components folder

--- Animalis/src/app/shopping/components/products/products.component.ts
+++ Animalis/src/app/shopping/components/products/products.component.ts
@@ -1,11 +1,11 @@
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../shared/services/product.service';
+import { ProductService } from '../../../shared/services/product.service';
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../shared/models/product';
+import { Product } from '../../../shared/models/product';
 import { switchMap } from 'rxjs/operators';
-import { ShoppingCartService } from '../shared/services/shopping-cart.service';
+import { ShoppingCartService } from '../../../shared/services/shopping-cart.service';
 import { Observable } from 'rxjs';
-import { ShoppingCart } from '../shared/models/shopping-cart';
+import { ShoppingCart } from '../../../shared/models/shopping-cart';
 
 @Component({
   selector: 'app-products',
