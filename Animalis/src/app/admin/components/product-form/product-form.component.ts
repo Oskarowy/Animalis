08da2511472190a2cd64af1fb78c2166da81e4bb
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-12-16 18:50:30 +0100

    Updated order confirmation after placing it

--- Animalis/src/app/admin/components/product-form/product-form.component.ts
+++ Animalis/src/app/admin/components/product-form/product-form.component.ts
@@ -10,7 +10,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
 categories$;
-product = {};
+product:any = {}; 
 id;
 
   constructor(
